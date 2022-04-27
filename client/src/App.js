22ee3b03b7daced9c3a5030a313ed545fe8bf850
tiigamarc <tiigamarc1@gmail.com>
2022-04-27 11:47:37 -0500

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,99 +1,71 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import './App.css'
-import {getWeb3} from "./getWeb3"
+import { getWeb3 } from "./getWeb3"
 import map from "./artifacts/deployments/map.json"
-import {getEthereum} from "./getEthereum"
-
+import { getEthereum } from "./getEthereum"
 class App extends Component {
-
     state = {
         web3: null,
         accounts: null,
         chainid: null,
-        vyperStorage: null,
-        vyperValue: 0,
-        vyperInput: 0,
-        solidityStorage: null,
-        solidityValue: 0,
-        solidityInput: 0,
+        simpleStorage: null,
+        storageValue: 0,
+        storageInput: 0,
+        transactionHash: null
     }
-
     componentDidMount = async () => {
-
         // Get network provider and web3 instance.
         const web3 = await getWeb3()
-
         // Try and enable accounts (connect metamask)
         try {
             const ethereum = await getEthereum()
-            ethereum.enable()
+            // ethereum.enable()
+            ethereum.request({ method: 'eth_requestAccounts' });
         } catch (e) {
-            console.log(`Could not enable accounts. Interaction with contracts not available.
+            console.log(`Could not enable accounts. 
+            Interaction with contracts not available.
             Use a modern browser with a Web3 plugin to fix this issue.`)
             console.log(e)
         }
-
-        // Use web3 to get the user's accounts
+        // Use web3 to get the users accounts
         const accounts = await web3.eth.getAccounts()
-
         // Get the current chain id
         const chainid = parseInt(await web3.eth.getChainId())
-
         this.setState({
             web3,
             accounts,
             chainid
         }, await this.loadInitialContracts)
-
     }
-
     loadInitialContracts = async () => {
-        // <=42 to exclude Kovan, <42 to include kovan
-        if (this.state.chainid < 42) {
-            // Wrong Network!
-            return
-        }
-        console.log(this.state.chainid)
-        
         var _chainID = 0;
-        if (this.state.chainid === 42){
-            _chainID = 42;
+        if (this.state.chainid === 3) {
+            _chainID = 3;
         }
-        if (this.state.chainid === 1337){
+        if (this.state.chainid === 1337) {
             _chainID = "dev"
         }
-        console.log(_chainID)
-        const vyperStorage = await this.loadContract(_chainID,"VyperStorage")
-        const solidityStorage = await this.loadContract(_chainID,"SolidityStorage")
-
-        if (!vyperStorage || !solidityStorage) {
+        const simpleStorage = await this.loadContract(_chainID, "SimpleStorage")
+        if (!simpleStorage) {
             return
         }
-
-        const vyperValue = await vyperStorage.methods.get().call()
-        const solidityValue = await solidityStorage.methods.get().call()
-
+        const storageValue = await simpleStorage.methods.get().call()
         this.setState({
-            vyperStorage,
-            vyperValue,
-            solidityStorage,
-            solidityValue,
+            simpleStorage,
+            storageValue,
         })
     }
-
     loadContract = async (chain, contractName) => {
         // Load a deployed contract instance into a web3 contract object
-        const {web3} = this.state
-
+        const { web3 } = this.state
         // Get the address of the most recent deployment from the deployment map
         let address
         try {
             address = map[chain][contractName][0]
         } catch (e) {
-            console.log(`Couldn't find any deployed contract "${contractName}" on the chain "${chain}".`)
+            console.log(`Could not find any deployed contract "${contractName}" on the chain "${chain}".`)
             return undefined
         }
-
         // Load the artifact with the specified address
         let contractArtifact
         try {
@@ -102,70 +74,43 @@ class App extends Component {
             console.log(`Failed to load contract artifact "./artifacts/deployments/${chain}/${address}.json"`)
             return undefined
         }
-
         return new web3.eth.Contract(contractArtifact.abi, address)
     }
-
-    changeVyper = async (e) => {
-        const {accounts, vyperStorage, vyperInput} = this.state
+    changeStorage = async (e) => {
+        const { accounts, simpleStorage, storageInput } = this.state
         e.preventDefault()
-        const value = parseInt(vyperInput)
+        const value = parseInt(storageInput)
         if (isNaN(value)) {
             alert("invalid value")
             return
         }
-        await vyperStorage.methods.set(value).send({from: accounts[0]})
+        await simpleStorage.methods.set(value).send({ from: accounts[0] })
+            .on('transactionHash', async (transactionHash) => {
+                this.setState({ transactionHash })
+            })
             .on('receipt', async () => {
                 this.setState({
-                    vyperValue: await vyperStorage.methods.get().call()
+                    storageValue: await simpleStorage.methods.get().call()
                 })
             })
     }
-
-    changeSolidity = async (e) => {
-        const {accounts, solidityStorage, solidityInput} = this.state
-        e.preventDefault()
-        const value = parseInt(solidityInput)
-        if (isNaN(value)) {
-            alert("invalid value")
-            return
-        }
-        await solidityStorage.methods.set(value).send({from: accounts[0]})
-            .on('receipt', async () => {
-                this.setState({
-                    solidityValue: await solidityStorage.methods.get().call()
-                })
-            })
-    }
-
     render() {
         const {
-            web3, accounts, chainid,
-            vyperStorage, vyperValue, vyperInput,
-            solidityStorage, solidityValue, solidityInput
+            web3,
+            accounts,
+            simpleStorage,
+            storageValue,
+            storageInput,
+            transactionHash
         } = this.state
-
         if (!web3) {
             return <div>Loading Web3, accounts, and contracts...</div>
         }
-
-        // <=42 to exclude Kovan, <42 to include Kovan
-        if (isNaN(chainid) || chainid < 42) {
-            return <div>Wrong Network! Switch to your local RPC "Localhost: 8545" in your Web3 provider (e.g. Metamask)</div>
-        }
-
-        if (!vyperStorage || !solidityStorage) {
+        if (!simpleStorage) {
             return <div>Could not find a deployed contract. Check console for details.</div>
         }
-
         const isAccountsUnlocked = accounts ? accounts.length > 0 : false
-
         return (<div className="App">
-            <h1>Your Brownie Mix is installed and ready.</h1>
-            <p>
-                If your contracts compiled and deployed successfully, you can see the current
-                storage values below.
-            </p>
             {
                 !isAccountsUnlocked ?
                     <p><strong>Connect with Metamask and refresh the page to
@@ -173,45 +118,31 @@ class App extends Component {
                     </p>
                     : null
             }
-            <h2>Vyper Storage Contract</h2>
-
-            <div>The stored value is: {vyperValue}</div>
-            <br/>
-            <form onSubmit={(e) => this.changeVyper(e)}>
+            <h1>Simple Storage</h1>
+            <div>The current stored value is {storageValue}.</div>
+            <br />
+            <form onSubmit={(e) => this.changeStorage(e)}>
                 <div>
-                    <label>Change the value to: </label>
-                    <br/>
+                    <label>Change the value to </label>
                     <input
-                        name="vyperInput"
+                        name="storageInput"
                         type="text"
-                        value={vyperInput}
-                        onChange={(e) => this.setState({vyperInput: e.target.value})}
-                    />
-                    <br/>
-                    <button type="submit" disabled={!isAccountsUnlocked}>Submit</button>
+                        value={storageInput}
+                        onChange={(e) => this.setState({ storageInput: e.target.value })}
+                    />.
+                    <p>
+                        <button type="submit" disabled={!isAccountsUnlocked}>Submit</button>
+                    </p>
                 </div>
             </form>
-
-            <h2>Solidity Storage Contract</h2>
-            <div>The stored value is: {solidityValue}</div>
-            <br/>
-            <form onSubmit={(e) => this.changeSolidity(e)}>
+            <br />
+            {transactionHash ?
                 <div>
-                    <label>Change the value to: </label>
-                    <br/>
-                    <input
-                        name="solidityInput"
-                        type="text"
-                        value={solidityInput}
-                        onChange={(e) => this.setState({solidityInput: e.target.value})}
-                    />
-                    <br/>
-                    <button type="submit" disabled={!isAccountsUnlocked}>Submit</button>
-
+                    <p>Last transaction Hash: {transactionHash}</p>
                 </div>
-            </form>
+                : null
+            }
         </div>)
     }
 }
-
 export default App
